c8ae15244d342a9f8408e86b7b2c28ce8c120c71
hungvu7122 <117963628+hungvu7122@users.noreply.github.com>
2026-08-19 15:34:34 +0700

    Add flotherm module initialization

---
+++ src/flotherm/__init__.py
@@ -0,0 +1,7 @@
+"""
+Flotherm integration module
+"""
+from src.flotherm.simulation import FlothermSimulation
+from src.flotherm.api_client import FlothermAPIClient
+
+__all__ = ['FlothermSimulation', 'FlothermAPIClient']
